27831a1da18e0440984fa129bb7f8a2db6bc4043
Indragith20 <indragith20@gmil.com>
2018-08-10 00:40:07 +0530

    make it

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/stats-screen'));
+app.use(express.static('./dist'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/stats-screen/index.html'));
+  res.sendFile(path.join('./dist/index.html'));
 });
 app.listen(process.env.PORT || 8080);
