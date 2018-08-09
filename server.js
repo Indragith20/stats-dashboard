e6e5c0860ecacf2b25dfc28ea7fd4aa86b4f8144
Indragith20 <indragith20@gmil.com>
2018-08-10 00:30:52 +0530

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-app.use(express.static(__dirname+'/dist'));
-
-app.listen(process.env.PORT||8080);
-
-
-//Path Location Strategy
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.use(express.static('./dist/stats-screen'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/stats-screen/index.html'));
 });
-
-console.log('Console Listening'); 
+app.listen(process.env.PORT || 8080);
